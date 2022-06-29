650a27fc788d0c710c1b2e10e3041f7bed01f03e
ZeeshanNSP <zeeshan20231@outlook.com>
2022-06-29 15:20:43 +0400

    290622

---
+++ main.py
@@ -0,0 +1,98 @@
+from ast import Load
+import json
+from re import L
+from socket import timeout
+import sys
+import time
+from datetime import datetime
+import os
+import pymongo
+from flask import Flask, render_template,redirect,request,jsonify,session,abort,send_from_directory
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+#configuration variables
+app.secret_key = "53C437"
+USERS = None
+TITLE = "NSP"
+# Methods and operation for the session and other utilities
+def sessionCheck():
+    if "user" in session:
+        return True
+    else:
+        return False
+def LoadUsers():
+    f = open("cred.json","r")
+    global USERS
+    USERS = json.load(f)
+    f.close()
+
+
+def updateCred():
+    f = open("cred.json","w")
+    f.write(json.dumps(USERS))
+    f.close()
+
+
+def currentUser():
+    if sessionCheck():
+        u = session["user"]
+        return USERS[u]  
+
+#routes for the web portal    
+@app.route("/logout",methods = ["GET"])
+def Logout():
+    session.pop("user")
+    return redirect("/")     
+
+
+@app.route("/profile",methods=["GET","POST"])
+def profile():
+    if sessionCheck():
+        if request.method =="GET":
+            return render_template("profile.html",title = TITLE,user = currentUser(),noti = None)
+        else:
+            return  render_template("profile.html",title= TITLE,user = currentUser(),noti = "Updated Successfully")
+    else:
+        return redirect("/")
+@app.route("/",methods = ["GET","POST"])
+def index():
+    if request.method == "GET":
+        if sessionCheck():
+            return render_template("index.html",title=TITLE)
+        else:
+            return render_template("login.html",title = TITLE)
+    if request.method == "POST":
+        user = request.form.get("user")
+        pswd = request.form.get("password")
+        if user in USERS:
+            if USERS[user]["password"] == pswd:
+                session["user"] = user
+                return "Logging you In"
+            else:
+                return "Invalid Credentials"
+        else:
+            return "Username Invalid"        
+        
+
+#Routes for the css,js and other image assets
+@app.route("/assets/<path:path>")
+def sendAssets(path):
+    return send_from_directory("assets/",path)
+@app.route("/css/<path:path>")
+def sendCss(path):
+    return send_from_directory("css/",path)
+@app.route("/vendors/<path:path>")
+def sendVendor(path):
+    return send_from_directory("vendors/",path)
+@app.route("/js/<path:path>")
+def sendJS(path):
+    return send_from_directory("js/",path)
+
+if __name__ == "__main__":
+    LoadUsers()
+    app.debug = True
+    app.run(host="0.0.0.0",port=443, use_reloader=True,ssl_context=("localhost+2.pem","localhost+2-key.pem"))
