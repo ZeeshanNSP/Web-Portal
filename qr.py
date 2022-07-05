f45a4fd96e286a47435516b414d46cb6a3d13c33
ZeeshanNSP <zeeshan20231@outlook.com>
2022-07-05 17:53:20 +0400

    050722

--- qr.py
+++ qr.py
@@ -1,5 +1,15 @@
 
 
+def getShortURL(url):
+    import requests
+    api_key = "672811c1f95e820df4623207537d289632358"
+    api_url = f"https://cutt.ly/api/api.php?key={api_key}&short={url}"
+    data = requests.get(api_url).json()["url"]
+    if data["status"] == 7:
+        return data["shortLink"]
+    else:
+        return url
+
 def QR(data):# Importing library
     import qrcode
     obj_qr = qrcode.QRCode(  
@@ -7,6 +17,7 @@ def QR(data):# Importing library
     error_correction = qrcode.constants.ERROR_CORRECT_L,  
     box_size = 10,  
     border = 0.1)
+    data = getShortURL(data)
     obj_qr.add_data(data)  
     obj_qr.make(fit = True)  
     qr_img = obj_qr.make_image(back_color=(255, 255, 255), fill_color=(200, 0, 0))  
